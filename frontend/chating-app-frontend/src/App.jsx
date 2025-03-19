0d8be3135f599e4524d5915a0e2f8af4df6392fa
Aminulhoque01 <aminuhoque53@gmail.com>
2025-03-19 15:28:01 +0600

    message from

--- frontend/chating-app-frontend/src/App.jsx
+++ frontend/chating-app-frontend/src/App.jsx
@@ -19,11 +19,11 @@ function App() {
 
   console.log({authUser});
 
-  if(isCheckingAuth && !authUser) return(
-    <div className='flex items-center justify-center h-screen'>
-      <Loader className='size-10 animate-spin'></Loader>
-    </div>
-  )
+  // if(isCheckingAuth && !authUser) return(
+  //   <div className='flex items-center justify-center h-screen'>
+  //     <Loader className='size-10 animate-spin'></Loader>
+  //   </div>
+  // )
   return (
     <div>
       <Navbar></Navbar>
